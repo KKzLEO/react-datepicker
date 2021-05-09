804bd0fd8cae7b06c668612c2fbdc7df0b2b8d12
KKzLEO <kkleo@ntut.org.tw>
2021-05-09 16:08:16 +0800

    feat(constants): add constants for calendar

---
+++ src/constants/calendar.js
@@ -0,0 +1,60 @@
+export const ROWS = 6
+export const COLUMNS = 7
+export const GRIDS = ROWS * COLUMNS
+
+export const MONTH_ROWS = 3
+export const MONTH_COLUMNS = 4
+export const MONTH_GRIDS = MONTH_ROWS * MONTH_COLUMNS
+
+export const YEAR_ROWS = 3
+export const YEAR_COLUMNS = 4
+export const YEAR_GRIDS = YEAR_ROWS * YEAR_COLUMNS
+
+export const MONTHS_HAVE_30_DAYS = [4, 6, 9, 11]
+export const MAX_YEARS = 2099
+export const MIN_YEARS = 1900
+
+export const WEEK_DAYS = {
+  SUNDAY: 0,
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6,
+}
+
+export const MONTHS = {
+  JAN: 'Jan',
+  FEB: 'Feb',
+  MAR: 'Mar',
+  APR: 'Apr',
+  MAY: 'May',
+  JUN: 'Jun',
+  JUL: 'Jul',
+  AUG: 'Aug',
+  SEP: 'Sep',
+  OCT: 'Oct',
+  NOV: 'Nov',
+  DEC: 'Dec',
+}
+
+const {
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+} = WEEK_DAYS
+
+export const MINI_NAME_OF_WEEK_MAP = {
+  [SUNDAY]: 'Su',
+  [MONDAY]: 'Mo',
+  [TUESDAY]: 'Tu',
+  [WEDNESDAY]: 'We',
+  [THURSDAY]: 'Th',
+  [FRIDAY]: 'Fr',
+  [SATURDAY]: 'Sa',
+}
