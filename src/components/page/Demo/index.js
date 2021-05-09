31ca0fe3327fe8a659bd6e0a40e9a253a5619582
Li, YinJun | Leo | TWR <yinjun.li@rakuten.com>
2021-05-10 02:32:25 +0800

    feat(demopage): more use cases

--- src/components/page/Demo/index.js
+++ src/components/page/Demo/index.js
@@ -4,22 +4,72 @@ import * as Style from './style'
 
 const FORMAT = 'YYYY-MM-DD'
 
-const DemoPage = (props) => {
-  const [birthday, setBirthday] = useState(null)
+const DemoPage = () => {
+  const [input, setInput] = useState(null)
 
   return (
     <Style.Container>
-      <DatePicker
-        onChange={setBirthday}
-        label="生日"
-        value={birthday}
-        format={FORMAT}
-        invalidFormatMessage={`格式必須為 ${FORMAT}`}
-      />
+      <Style.Group>
+        <DatePicker
+          onChange={setInput}
+          label="可以手動輸入"
+          value={input}
+          format={FORMAT}
+          invalidFormatMessage={`格式必須為 ${FORMAT}`}
+          canInput
+        />
+      </Style.Group>
+      <Style.Group>
+        <DatePicker
+          onChange={setInput}
+          label="不可以手動輸入"
+          value={input}
+          format={FORMAT}
+          invalidFormatMessage={`格式必須為 ${FORMAT}`}
+        />
+      </Style.Group>
+      <Style.Group>
+        <DatePicker
+          onChange={setInput}
+          label="可以自定義 format, MM/DD/YYYY"
+          value={input}
+          format="MM/DD/YYYY"
+          invalidFormatMessage={`格式必須為 MM/DD/YYYY`}
+        />
+      </Style.Group>
+      <Style.Group>
+        <DatePicker
+          onChange={setInput}
+          label="MM/DD/YYYY 不是這個 format 都會跳出 invalid format"
+          value={input}
+          format="MM/DD/YYYY"
+          invalidFormatMessage={`格式必須為 MM/DD/YYYY`}
+          canInput
+        />
+      </Style.Group>
+      <Style.Group>
+        <DatePicker
+          onChange={setInput}
+          label="自定義錯誤"
+          value={input}
+          format={FORMAT}
+          invalidFormatMessage={`格式必須為 ${FORMAT}`}
+          hasError
+          helperText="自定義的錯誤訊息"
+        />
+      </Style.Group>
+      <Style.Group>
+        <DatePicker
+          onChange={setInput}
+          label="有 helperText"
+          value={input}
+          format={FORMAT}
+          invalidFormatMessage={`格式必須為 ${FORMAT}`}
+          helperText="我是 helper text"
+        />
+      </Style.Group>
     </Style.Container>
   )
 }
-
-DemoPage.propTypes = {}
 
 export default DemoPage
--- src/components/page/Demo/style.js
+++ src/components/page/Demo/style.js
@@ -2,8 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100vw;
   height: 100vh;
+`
+
+export const Group = styled.div`
+  margin-top: 20px;
 `
