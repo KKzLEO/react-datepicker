aba395678ba8e5e88088d720f0d9f0a97421452f
Li, YinJun | Leo | TWR <yinjun.li@rakuten.com>
2021-05-10 02:29:34 +0800

    feat(baseutils): implement `getUrlParams` utils

--- src/utils/base.js
+++ src/utils/base.js
@@ -5,3 +5,21 @@ export const padStart = (string, targetLength, padString = ' ') => {
 export const isNil = (value) => value === null || value === undefined
 
 export const noop = () => {}
+
+export const getUrlParams = (location = window.location) => {
+  const { search } = location
+
+  if (isNil(search)) return {}
+
+  return search
+    .substring(1)
+    .split('&')
+    .reduce((acc, param) => {
+      const [key, value] = param.split('=')
+
+      return {
+        ...acc,
+        [key]: value,
+      }
+    }, {})
+}
